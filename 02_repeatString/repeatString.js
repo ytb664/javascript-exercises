a47b170c0b81fdcb792bec86fed6366bc8588472
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-08 13:09:45 +0700

    Finish 02_repeatString exc

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inputString, repeatNumber) {
 
+    if (repeatNumber < 0) {
+        return "ERROR";
+    }
+
+    let result = "";
+    for (let i = 0; i < repeatNumber; i++) {
+        result += inputString;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
