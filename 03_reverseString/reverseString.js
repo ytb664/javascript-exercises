90b2f67d5045aae02962577fc2f27e420e480299
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-09 09:08:56 +0700

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,12 @@
 const reverseString = function(string) {
 
     let reversedString = '';
-    
+    let stringLastIndex = string.length - 1;
+    for (let i = stringLastIndex; i >= 0; i--) {
+        reversedString += string.slice(i, i + 1);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
