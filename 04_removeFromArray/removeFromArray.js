b2b103c88595f86d6f95ff304988426e46bd3a4b
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-10 15:35:46 +0700

    Add slice method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,22 +2,23 @@ const removeFromArray = function() {
 
     let argLastIndex = arguments.length - 1;
     let arrayArgument = arguments[0];
+    let endArray = [];
     // Loop from the last argument and store the value to variable
     for (let i = argLastIndex; i > 0; i--) {
 
+        if (argLastIndex === 0) break; 
         let valueToRemove = arguments[i];
         // Comparing each element in the array with the stored value
-        for (let j = 0; j > arrayArgument.length; j--) {
+        for (let j = 0; j < arrayArgument.length; j++) {
             if (arrayArgument[j] != valueToRemove) {
                 continue;
             }
 
-            arrayArgument.splice(j);
-            break;
+            endArray = arrayArgument.slice(j, 1);
         }
-
-        return arrayArgument;
     }
+
+    return endArray;
 };
 
 // Do not edit below this line
