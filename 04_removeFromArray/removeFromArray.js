f14b154462ea5a66f5ecfcdb37a68101da65d928
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-10 14:18:33 +0700

    Fix infinite loop bug

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
 
+    let argLastIndex = arguments.length - 1;
+    let arrayArgument = arguments[0];
+    // Loop from the last argument and store the value to variable
+    for (let i = argLastIndex; i > 0; i--) {
+
+        let valueToRemove = arguments[i];
+        // Comparing each element in the array with the stored value
+        for (let j = 0; j > arrayArgument.length; j--) {
+            if (arrayArgument[j] != valueToRemove) {
+                continue;
+            }
+
+            arrayArgument.splice(j);
+            break;
+        }
+
+        return arrayArgument;
+    }
 };
 
 // Do not edit below this line
