257be363745916f13fa50a119fe7cf7e054b733f
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-12 13:54:39 +0700

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(theFahrenheit) {
+
+  const x = theFahrenheit;
+
+  let result = (x - 32) * (5 / 9);
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(theCelsius) {
+
+  const x = theCelsius;
+
+  let result = (x * (9 / 5) + 32);
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
