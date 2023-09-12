2a3f3174da05b3c4d2c318f98e82fef7b75acafb
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-12 08:34:19 +0700

    Finish 3 test
    still failed in the rest 3 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
 const sumAll = function() {
 
+    let sum = 0;
+    const startNum = arguments[0];
+    const endNum = arguments[1];
+
+    if (startNum < endNum) {
+
+        for (let i = startNum; i <= endNum; i++) {
+            sum += i;
+        }    
+    } else if (startNum > endNum) {
+        
+        for (let i = endNum; i >= startNum; i--) {
+            sum -= i;
+        }
+    } else if (startNum < 0 || endNum < 0) {
+        return "ERROR";
+    } else if (typeof startNum != Number || typeof endNum != Number) {
+        return "ERROR";
+    }
+
+
+    return sum;
 };
 
 // Do not edit below this line
