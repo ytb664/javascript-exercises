e8b0bb9cd3c02209d33acf2844835f462410533f
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-12 13:44:23 +0700

    Finish 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(isLeap) {
 
+    if (isLeap % 4 == 0 && isLeap % 100 != 0) {
+        return true;
+    } else if (isLeap % 100 === 0 &&  isLeap % 400 === 0) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
