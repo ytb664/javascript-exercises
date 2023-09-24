871da0772e2bfb40363664e8bcc1e2c45ea9fb54
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-24 17:23:03 +0700

    Finish subtract 08

--- 08_calculator/calculator.spec.js
+++ 08_calculator/calculator.spec.js
@@ -15,7 +15,7 @@ describe('add', () => {
 });
 
 describe('subtract', () => {
-  test.skip('subtracts numbers', () => {
+  test('subtracts numbers', () => {
     expect(calculator.subtract(10, 4)).toBe(6);
   });
 });
