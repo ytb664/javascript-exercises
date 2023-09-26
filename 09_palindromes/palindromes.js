28a0b12070623591188ba0f73a3b31ca54bf38e5
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-26 14:49:54 +0700

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,42 @@
-const palindromes = function () {
+const palindromes = function (inputtedString) {
 
+    let comparison = splitAndReverse(inputtedString);
+    inputtedString = splitStr(inputtedString);
+    inputtedString = standardizeStr(inputtedString);
+
+    function splitStr(str) {
+        let result = str.toLowerCase().split('');
+
+        return result;
+    }
+
+    function splitAndReverse(str) {
+        let result = splitStr(str).reverse();
+
+        return standardizeStr(result);
+    }
+
+    function standardizeStr(str) {
+        let result = str.filter(item => item !== ' ' &&
+            item !== ',' && item !== '.' && item !== '!');
+        result = result.join('');
+        result = result.toLowerCase();
+        removePunctuation(result);
+
+        return result;
+    }
+
+    function removePunctuation(string) {
+
+        string = string.split('');
+        string = string.filter(item => item !== '!' &&
+            item !== ',' || item !== ' ' ||
+            item !== '.').join('');
+
+        return string;
+    }
+
+    return comparison === inputtedString;
 };
 
 // Do not edit below this line
