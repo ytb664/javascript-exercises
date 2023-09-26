1bca0ad31d4b364f880bf599e5ab0138bccc6e34
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-26 16:19:51 +0700

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0) return "OOPS";
+
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 1; i < n; i++) {
+        let temp = num1;
+        num1 = num2;
+        num2 = num1 + temp;
+    }
+
+    return num2;
 };
 
 // Do not edit below this line
