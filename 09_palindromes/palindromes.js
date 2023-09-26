e95f3d383373af29db5e6b392ada9805442ef422
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-26 14:52:41 +0700

    Change to the solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,42 +1,7 @@
-const palindromes = function (inputtedString) {
+const palindromes = function (string) {
 
-    let comparison = splitAndReverse(inputtedString);
-    inputtedString = splitStr(inputtedString);
-    inputtedString = standardizeStr(inputtedString);
-
-    function splitStr(str) {
-        let result = str.toLowerCase().split('');
-
-        return result;
-    }
-
-    function splitAndReverse(str) {
-        let result = splitStr(str).reverse();
-
-        return standardizeStr(result);
-    }
-
-    function standardizeStr(str) {
-        let result = str.filter(item => item !== ' ' &&
-            item !== ',' && item !== '.' && item !== '!');
-        result = result.join('');
-        result = result.toLowerCase();
-        removePunctuation(result);
-
-        return result;
-    }
-
-    function removePunctuation(string) {
-
-        string = string.split('');
-        string = string.filter(item => item !== '!' &&
-            item !== ',' || item !== ' ' ||
-            item !== '.').join('');
-
-        return string;
-    }
-
-    return comparison === inputtedString;
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
