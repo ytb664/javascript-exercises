64fa605cf1c4a72596322d5cc97a1c701d3bc178
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-26 22:11:34 +0700

    Finish 1 in the 12th

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    for (let el of people) {
+        el.age = el.yearOfDeath - el.yearOfBirth;
+    }
+
+    function getFirstAge(arr) {
+        this[0].age;
+    }
+    let firstAge = people[0].age;
+    let result = people.filter(item => item.age > firstAge);
+
+
+    result = result.filter(item => item.age >= result[0].age);
+
+    return result[0];
 };
 
 // Do not edit below this line
